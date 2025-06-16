10333a890f50d40ae489a144bfc9cdf2bd3213ae
Pawel Walicki <pawel.walicki89@interia.pl>
2025-06-16 17:09:21 +0200

    Logging with google

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,8 @@
 import { Button, Container, TextField, Grid } from "@mui/material";
+import GoogleButton from "react-google-button";
 import { useState } from "react"
-import { auth } from "../firebase";
-import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth, googleProvider } from "../firebase";
+import { signInWithEmailAndPassword, signInWithPopup, signOut, sendPasswordResetEmail } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import './LoginForm.css'
 import { RegisterLink } from "./RegisterLink";
@@ -32,6 +33,26 @@ export const LoginForm = () => {
     const handleKeyPress = (e) => {
         if (e.key === "Enter") {
             loginWithUsernameAndPassword(e)
+        }
+    }
+
+    const loginWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider)
+            navigate("/")
+        } catch (error) {
+            console.error(error)
+            setNotice("logging with google failed")
+        }
+    }
+
+    const resetPassword = async (e) => {
+        e.preventDefault()
+        try {
+            await sendPasswordResetEmail(auth, email)
+            setNotice("Mail to reset password is sent!")
+        }catch(error) {
+            setNotice("We cannot reset your password!")
         }
     }
 
@@ -98,6 +119,15 @@ export const LoginForm = () => {
                         color: "#ffffff",
                     }}>Submit</Button>
                 </Grid>
+                <Button onClick={(e) => resetPassword(e)}>
+                    Password reset!
+                </Button>
+                <Grid display="flex" justifyContent="center" alignItems="center" size={7}>
+                    <GoogleButton
+                        onClick={loginWithGoogle}
+                    />
+                </Grid>
+
             </Grid>
             <Grid display="flex" justifyContent="center" alignItems="center" size={7} paddingTop={"10px"}>
                 <RegisterLink />
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database"
-import {getAuth} from "firebase/auth"
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -24,3 +24,4 @@ const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getDatabase(app)
 export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
